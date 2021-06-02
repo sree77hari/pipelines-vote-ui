e1c99aee3cd07543871023dd3fd88baa076ac097
Nikhil Thomas <nikhilthomas1@gmail.com>
2021-06-02 11:29:41 +0530

    Set default host and port

--- app.py
+++ app.py
@@ -16,7 +16,9 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST','GET'])
 def hello():
-    rest_endpoint="http://" + os.environ["VOTING_API_SERVICE_HOST"] + ":" + os.environ["VOTING_API_SERVICE_PORT"]
+    host = os.getenv("VOTING_API_SERVICE_HOST", "pipelines-vote-api")
+    port = os.getenv("VOTING_API_SERVICE_PORT", "8080")
+    rest_endpoint="http://" + host + ":" + port
     voter_id = request.cookies.get('voter_id')
     if not voter_id:
         voter_id = hex(random.getrandbits(64))[2:-1]
